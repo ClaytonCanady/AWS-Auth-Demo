59fbd97dad8a1a94d38316cd55240e4758c6df46
Clayton C <67292469+ClaytonCanady@users.noreply.github.com>
2025-04-16 08:53:53 -0500

    prepare for deployment

---
+++ backend/lib/backend-stack.ts
@@ -0,0 +1,36 @@
+import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import { Construct } from 'constructs';
+
+export class BackendStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // Create Lambda function
+    const handler = new lambda.Function(this, 'BackendHandler', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      code: lambda.Code.fromAsset('src'),
+      handler: 'index.handler',
+      environment: {
+        // Add your environment variables here
+      }
+    });
+
+    // Create API Gateway
+    const api = new apigateway.RestApi(this, 'BackendApi', {
+      restApiName: 'Backend Service',
+      description: 'API Gateway for the backend service'
+    });
+
+    // Create API Gateway integration
+    const integration = new apigateway.LambdaIntegration(handler);
+
+    // Add routes
+    api.root.addMethod('ANY', integration);
+    api.root.addProxy({
+      defaultIntegration: integration,
+      anyMethod: true
+    });
+  }
+} 
---
+++ frontend/lib/frontend-stack.ts
@@ -0,0 +1,52 @@
+import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+import { Construct } from 'constructs';
+
+export class FrontendStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // Create S3 bucket for website hosting
+    const websiteBucket = new s3.Bucket(this, 'WebsiteBucket', {
+      websiteIndexDocument: 'index.html',
+      websiteErrorDocument: 'index.html',
+      publicReadAccess: false,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+
+    // Create CloudFront distribution
+    const distribution = new cloudfront.Distribution(this, 'Distribution', {
+      defaultBehavior: {
+        origin: new origins.S3Origin(websiteBucket),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+      defaultRootObject: 'index.html',
+      errorResponses: [
+        {
+          httpStatus: 404,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html',
+        },
+      ],
+    });
+
+    // Deploy website contents to S3
+    new s3deploy.BucketDeployment(this, 'DeployWebsite', {
+      sources: [s3deploy.Source.asset('dist')],
+      destinationBucket: websiteBucket,
+      distribution,
+      distributionPaths: ['/*'],
+    });
+
+    // Output the CloudFront URL
+    new cdk.CfnOutput(this, 'DistributionDomainName', {
+      value: distribution.distributionDomainName,
+      description: 'The CloudFront distribution domain name',
+    });
+  }
+} 
